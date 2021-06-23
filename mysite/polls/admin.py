300191dff93bd1689347936c0afe52dec75b4b3c
변찬우 <chan00106@naver.com>
2021-06-23 16:54:59 +0900

    finish!!

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
 # Register your models here.
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['text']}),
+        ('Date information', {'fields': ['date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('text', 'date')
+    search_fields = ['text']
+
+admin.site.register(Question, QuestionAdmin)
